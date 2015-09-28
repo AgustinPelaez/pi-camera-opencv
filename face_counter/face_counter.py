640856b7b847635f40f850b7f7fbf44375d5e207
Gustavo Andrés Angulo <woakas@gmail.com>
2015-09-28 17:42:11 -0500

    Changes for post data to ubidots

--- face_counter/face_counter.py
+++ face_counter/face_counter.py
@@ -1,50 +1,69 @@
+from flask import Flask, request
+from flask import jsonify
+
 import os
 import cv2
 import tempfile
+import requests
 
-from flask import Flask, request, redirect, url_for
-from flask import jsonify
-from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/tmp/'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+API_URL_UBIDOTS = 'http://things.ubidots.com/api/v1.6'
 FACE_CASCADE = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/images/face_counter", methods=['GET', 'POST'])
-def index():
+
+@app.route("/images/face_counter/<string:variable>/<string:token>", methods=['GET', 'POST'])
+def index(variable, token):
     if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+        rsp = requests.get(
+            '{api_url}/variables/{variable}'.format(api_url=API_URL_UBIDOTS, variable=variable),
+            headers={'x-auth-token': token}
+        )
+
+        # if the token not authorized or the variable does not exist
+        if rsp.status_code != 200:
+            return jsonify({"error": rsp.json()})
+
+        file_req = request.files['file']
+        if file_req and allowed_file(file_req.filename):
             f = tempfile.NamedTemporaryFile(delete=False)
-            file.save(f.name)
-            
+            file_req.save(f.name)
+
             # Read the image
             image = cv2.imread(f.name)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
             # Detect faces in the image
             faces = FACE_CASCADE.detectMultiScale(
-               gray,
-               scaleFactor=1.1,
-               minNeighbors=5,
-               minSize=(30, 30),
-               flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
             )
+
             os.unlink(f.name)
-            return jsonify({"faces": len(faces)})
+            rsp = requests.post(
+                '{api_url}/variables/{variable}/values'.format(api_url=API_URL_UBIDOTS, variable=variable),
+                data={'value': len(faces)},
+                headers={'x-auth-token': token}
+            )
+
+            return jsonify({"faces": len(faces), "api": rsp.json()})
         else:
             return jsonify({"error": 'extension not allowed'})
-             
 
     return jsonify({"usage": "POST /face_counter/"})
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
-
